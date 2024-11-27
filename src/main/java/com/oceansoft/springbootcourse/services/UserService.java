15132d156ebd5466b5b1bda6e10866803863efd9
Leo-Luz-code <leonardoluz768@gmail.com>
2024-11-27 03:06:06 -0300

    feat: add UserService and Component registration

---
+++ src/main/java/com/oceansoft/springbootcourse/services/UserService.java
@@ -0,0 +1,29 @@
+package com.oceansoft.springbootcourse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oceansoft.springbootcourse.entities.User;
+import com.oceansoft.springbootcourse.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+
+        Optional<User> user = userRepository.findById(id);
+
+        return user.get();
+
+    }
+}
